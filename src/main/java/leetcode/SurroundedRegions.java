7d89442cc7e4e846eca28fe986a22b8ee4e6c46f
Lee Fowler <fowler.lee8@gmail.com>
2024-09-29 21:50:13 +0200

    add surrounded regions medium algo

---
+++ src/main/java/leetcode/SurroundedRegions.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+public class SurroundedRegions {
+
+    private static void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#' || board[i][j] == 'X') {
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+
+    public static void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') {
+                    if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                        dfs(board, i, j);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][] {
+                { 'X', 'X', 'X', 'X' },
+                { 'X', 'O', 'O', 'X' },
+                { 'X', 'X', 'O', 'X' },
+                { 'X', 'O', 'X', 'X' } };
+        SurroundedRegions.solve(board);
+
+    }
+}
