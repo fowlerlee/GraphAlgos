b6ad420e6118add4cd629e9a01f0cbefecb61146
Lee Fowler <fowler.lee8@gmail.com>
2024-09-30 22:10:12 +0200

    loop chars for redistribute chars

--- src/main/java/leetcode/RedistributeChars.java
+++ src/main/java/leetcode/RedistributeChars.java
@@ -2,19 +2,27 @@ package leetcode;
 
 public class RedistributeChars {
     public static boolean makeEqual(String[] words) {
-        for (int i = 0; i < words.length; i++) {
-            String s = words[0]; // abc
-            char[] chars = s.toCharArray();
-            
+        int charArray[] = new int[26];
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            int length = words[i].length();
+            for (int j = 0; j < length; j++) {
+                char ch = words[i].charAt(j);
+                charArray[ch - 'a']++;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (charArray[i] % n != 0)
+                return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String[] words = new String[]{"abc","aabc","bc"};
+        String[] words = new String[] { "abc", "aabc", "bc" };
         boolean val = RedistributeChars.makeEqual(words);
         if (val) {
-            System.out.println("passed test");;
+            System.out.println("passed test");
         } else {
             System.err.println("failed test");
         }
